d7545634af5d2fec3a1bd31e5fc2dab00bbabae9
komalsahu407 <komalsahu58590@gmail.com>
2025-05-24 12:01:45 +0530

    Update Associativityoperators.java

--- Associativityoperators.java
+++ Associativityoperators.java
@@ -14,7 +14,7 @@ public class Associativityoperators {
         int a=5;
         int b=4;
         int c=3;
-        int k=b*b-(4*a*c)/(2*a);
+        int k=(b*b-4*a*c)/(2*a);
         System.out.println(k); 
     }
 }
